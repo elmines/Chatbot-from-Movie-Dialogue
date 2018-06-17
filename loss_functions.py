23ab59dbb3e85fe59f9b6bba94ce9751ff2cf6d7
Vidhushini.S <vidhushinisrinivasan@gmail.com>
2018-06-17 18:10:48 -0400

    Integrate loss functions
    Integrate loss functions

--- loss_functions.py
+++ loss_functions.py
@@ -22,7 +22,7 @@ def min_affective_dissonance(param,train_logits,targets,target_lengths,enc_embed
 def max_affective_dissonance(param,train_logits,targets,target_lengths,enc_embed_input,dec_embed_input):
     lamba = tf.Variable(param, name="lamba")
     lamba_inv=tf.Variable((1.0-param),name="lamba_inv")
-    predicted_prob = tf.reduce_max(dec_embed_input)
+    predicted_prob = tf.reduce_max(dec_embed_input,1)
       
     affect_distance = tf.nn.l2_normalize(tf.subtract(tf.reduce_mean(enc_embed_input,1), tf.reduce_mean(dec_embed_input,1)))
     
@@ -42,3 +42,7 @@ def max_affective_content(param,train_logits,targets,target_lengths,dec_embed_in
   
     final_cost = tf.subtract(tf.multiply(lamba_inv,cross_entropy(train_logits,targets,target_lengths)),val)
     return final_cost
+
+
+
+
