256a0d43cbe56d4cd8e5b0d3a4cf7cc7ee51dd85
Ethan Mines <elmines325@gmail.com>
2018-07-25 12:17:27 -0400

    Document config.py

--- config.py
+++ config.py
@@ -12,6 +12,8 @@ import experiment
 class Setting(object):
 	"""
 	A setting/parameter that can be set in a YAML configuration file.
+
+	:type name: str
 	"""
 
 	def __init__(self, name, type_fn, default, dependencies=None):
@@ -19,6 +21,7 @@ class Setting(object):
 		:param str         name: Name of the setting
 		:param callable type_fn: Function that transforms the YAML value to the desired type/object
 		:param callable default: Supplier function that takes no parameters and returns a default value
+		:param list(Setting) dependencies: A list of settings that must first be set before this Setting may be set
 		"""
 		self._name = name
 		self._type_fn = type_fn
@@ -31,25 +34,25 @@ class Setting(object):
 	@property
 	def name(self):
 		"""
-		The name of the Setting
+		str: The name of the Setting
 		"""
 		return self._name
 	@property
 	def type_fn(self):
 		"""
-		A function with signature type_fn(yaml_value) -> converted_value
+		callable: A function with signature type_fn(yaml_value) -> converted_value
 		"""
 		return self._type_fn
 	@property
 	def default(self):
 		"""
-		A callable that takes no parameters and returns a default value (still passed into type_fn later)
+		callable: A supplier function that takes no parameters and returns a default value (still passed into type_fn later)
 		"""
 		return self._default
 	@property
 	def dependencies(self):
 		"""
-		A list of Setting objects that must first be set before this Setting may be set
+		list(Setting): Settings that must first be set before this Setting may be set
 		"""
 		return self._dependencies
 
@@ -62,12 +65,46 @@ def _maybe_abspath(path):
 	return os.path.abspath(path) if path is not None else path
 
 def _maybe_str(candidate):
+	"""
+	:param str candidate: Either a string or None
+	:returns A string if candidate isn't None, otherwise None
+	"""
 	return str(candidate) if candidate is not None else candidate
 
 
 class Config(object):
+	"""
+	A namespace for the various settings of the experiment
 
+	:ivar int                 num_layers: The number of layers (for both the encoder and decoder); default 1
+	:ivar int                   rnn_size: The size of an RNN cell's hidden state; default 1024
+	:ivar int                  attn_size: The size of the attention mechanism; default 256
+	:ivar float            learning_rate: The step size for gradient-based optimizers; default 0.0001
+	:ivar int                 beam_width: The number of beams to generate when doing decoding; default 1 (equivalent to greedy decoding)
+	:ivar float      gradient_clip_value: Magnitude used to clip gradients during learning, such that gradients are clipped to [-gradient_clip_value, gradient_clip_value]; default 5.0
+	:ivar int                 max_epochs: Maximum number of epochs for training; default 10
+	:ivar int           train_batch_size: Minibatch size during training; default 64
+	:ivar int           infer_batch_size: Minibatch size during validation, testing, etc.; default 32
+	:ivar path-like           train_save: Path prefix at which to save training graphs during training; default `None`
+	:ivar path-like           infer_save: Path prefix at which to save inference graphs during training; default `None`
+	:ivar path-like           model_load: Path to a tf.train.Checkpoint file (used for continuing training or loading a model for inference); default `None`
+	:ivar str                       arch: Type of architecture, valid values are \"vad\" (for :py:class:`experiment.VADExp`) and \"distributed\" (for :py:class:`experiment.Aff2VecExp`); default \"distributed\"
+	:ivar path-like           embeddings: Path to a Numpy file (i.e. .npy or .npz) containing the embeddings for the model; default \"word_Vecs_VAD.npy\" or \"word_Vecs_retrofit_affect.npy\", depending on **arch**
+	:ivar path-like             data_dir: Path to the directory where train_prompts.txt, ..., vocab.txt reside; default \"corpora/\"
+	:ivar path-like           infer_text: Path to a text file with prompts separated by newlines; default `None`
+	:ivar path-like          infer_sheet: .xlsx file where all the prompts are in a single column; default `None`
+	:ivar str                  sheet_col: Column of **infer_sheet** from which to pull prompts; default `None` (caller can decide how to handle this case)
+	:ivar path-like            infer_out: .xlsx file to which the model can write its responses during inference; default `None`
+
+	"""
 	def __init__(self, config_file=None):	
+		"""
+		:param path-like config_file: YAML configuration file
+
+		If config_file isn't specified, `Setting.default` will be used for every Setting
+		Note that most scripts require certain Settings to be explicitly specified.
+		"""
+
 		self._config_file = config_file
 		self._define_settings()
 		self.__setattr__ = self._setattr_base #Override __setattr__ after assigning _setting_dict
@@ -86,8 +123,7 @@ class Config(object):
 		#Function used to override __setattr__
 		"""
 		:param str name: One of the object's members
-		:param str value: The member's value (to be converted with a type function)
-	
+		:param value: The member's value (to be converted with a type function)
 		"""
 		if name not in self._setting_dict:
 			raise KeyError("{} is not a valid setting.".format(name))
@@ -96,6 +132,9 @@ class Config(object):
 		super(Config, self).__setattr__(name, setting.type_fn(value))
 
 	def _define_settings(self):
+		"""
+		Subroutine that initializes a Config object's _setting_dict, which holds all its public memmbers
+		"""
 
 		_settings = []
 
@@ -147,6 +186,11 @@ class Config(object):
 
 	#TODO: Check for cyclical dependencies among Settings
 	def _initialize_settings(self, yaml_dict):
+		"""
+		:param dict(str, object) yaml_dict: Dictionary read from a YAML file
+		
+		Initializes all the settings (i.e. public members) of self
+		"""
 		#Sort of a memoization table that records which settings have already been computed
 		initialized = {name:False for name in self._setting_dict}
 		for setting in self._setting_dict.values():
@@ -158,9 +202,12 @@ class Config(object):
 
 	def _initialize_setting(self, setting, yaml_dict, initialized):
 		"""
-		:param Setting setting
-		:param dict(object, object) yaml_dict
-		:param dict(str, bool) initialized
+		:param Setting setting: Setting to initialize
+		:param dict(object, object) yaml_dict: Dictionary read from a YAML file
+		:param dict(str, bool) initialized: Dictionary indicating by name which Settings have already been initialized
+
+		The function recurs if setting has any dependencies.
+		**initialized** is used to determine when the base case has been reached.
 		"""
 		if initialized[setting.name]:
 			return	
@@ -171,15 +218,4 @@ class Config(object):
 
 		self.__setattr__(setting.name, yaml_dict.get(setting.name, setting.default()))
 		initialized[setting.name] = True
-
-
-if __name__ == "__main__":
-	#config = Config("config.yml")
-	config = Config()
-	print(config.num_layers)
-	print(config.learning_rate)
-
-	#config.learning_rate = "0.004"
-	#print(config.learning_rate)
-	#print(type(config.learning_rate))
 
