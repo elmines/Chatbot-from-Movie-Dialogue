3a2433b42b8ec5ed262ffff45b1e15424433458f
Ethan Mines <elmines325@gmail.com>
2018-08-04 14:22:27 -0500

    Move VAD dict to resources/; fix potential bug in generating Word2Vec embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,23 +1,47 @@
 """
-Utility module for generating word embeddings
+Utility module/script for generating word embeddings
 """
+#System utilities
+import sys
+import os
+import argparse
+
+#Computation
 import gensim
 import numpy as np
 import pandas as pd
 
-import sys
-
 #Local modules
 import match
 
-def w2vec(model_path, text, vocab2int, embedding_size=1024, verbose=True):
+def create_parser():
+	parser = argparse.ArgumentParser(description="Generate word embeddings given a corpus and vocabulary")
+
+	parser.add_argument("--vocab", "-v", required=True, metavar="vocab.txt", help="Vocabulary file where each line is"
+                                                                    " a word and its integer index, like so: \"jelly 272\"")
+	parser.add_argument("--save-path", required=True, metavar="new_embeddings.npy", help="Path at which to save the newly generated embeddings as a .npy file")
+
+	parser.add_argument("--size", default=1024, type=int, metavar="N", help="Embedding size for Word2Vec embeddings")
+	parser.add_argument("--w2v", metavar="corpus.txt", help="Generate Word2Vec embeddings from the corpus")
+
+	parser.add_argument("--retrofit", metavar="embeddings.npy", help="Retrofit existing word embeddings stored in a .npy or .npz file")
+	parser.add_argument("--counterfit", metavar="embeddings.npy", help="Counterfit existing word embeddings stored in a .npy or .npz file")
+
+
+	return parser
+
+_VAD_spreadsheet = os.path.join("resources", "Warriner, Kuperman, Brysbaert - 2013 BRM-ANEW expanded.xlsx")
+if not os.path.exists(_VAD_spreadsheet):
+	raise OSError("{} was not found and is a required resource for generating affective embeddings.".format(_VAD_spreadsheet))
+
+def w2vec(text, vocab2int, embedding_size=1024, verbose=True):
 	"""
 	Generates Word2Vec embeddings
 
-	:param path-like       model_path: The path at which to save the embeddings before returning
-	:param list(list(str))       text: The tokens from which to generate the embeddings, where text[i][j] is the jth token of the ith sequence
-	:param dict(str,int)    vocab2int: A mapping from tokens in the vocabulary to their integer indices
-	:param bool               verbose: Print helpful messages to stderr
+	:param list(list(str))          text: Tokenized sentences from which to generate the embeddings
+	:param dict(str,int)       vocab2int: A mapping from tokens in the vocabulary to their integer indices in the range [0, vocab_size)
+	:param int            embedding_size: The number of dimensions for each word embedding
+	:param bool                  verbose: Print helpful messages to stderr
 	
 	:returns: The generated embeddings
 	:rtype:   np.ndarray
@@ -25,20 +49,14 @@ def w2vec(model_path, text, vocab2int, embedding_size=1024, verbose=True):
 	if verbose: sys.stderr.write("Learning Word2Vec embeddings on {} sequences . . .\n".format(len(text)))
 	model = gensim.models.Word2Vec(sentences=text, size=embedding_size, window=5, min_count=1, workers=4, sg=0)
 	word_vecs = np.zeros((len(model.wv.vocab),embedding_size))
-	for i,word in enumerate(model.wv.index2word):
-        	word_vecs[vocab2int[word]] = model[word]
-
-	np.save(model_path,word_vecs)
-	if verbose: sys.stderr.write("Wrote Word2Vec model to {}\n".format(model_path))
-
+	for (word, index) in vocab2int.items():
+		word_vecs[index] = model[word]
 	return word_vecs
 
-
-def appended_vad(model_path, embeddings, vocab2int, exclude=None, verbose=True):
+def appended_vad(embeddings, vocab2int, exclude=None, verbose=True):
 	"""
 	Appends VAD (Valence, Arousal, Dominance) values to existing word embeddings
 
-	:param path-like       model_path: The path at which to save the new embeddings before returning
 	:param np.ndarray      embeddings: The original embeddings
 	:param dict(str,int)    vocab2int: A mapping from tokens in the vocabulary to their integer indices
 	:param list(str)          exclude: A list of tokens in vocab2int for which to assign the neutral vector (such as the unknown token)
@@ -50,7 +68,7 @@ def appended_vad(model_path, embeddings, vocab2int, exclude=None, verbose=True):
 	#Simple list of vocabulary items at their proper indices
 	int2vocab = sorted(vocab2int.keys(), key=vocab2int.__getitem__)
 
-	df_vad=pd.read_excel('Warriner, Kuperman, Brysbaert - 2013 BRM-ANEW expanded.xlsx')
+	df_vad=pd.read_excel(_VAD_spreadsheet)
 	df_vad["Word"] = df_vad["Word"].apply(str)
 	targ_vocab = list(df_vad["Word"])
 
@@ -80,8 +98,6 @@ def appended_vad(model_path, embeddings, vocab2int, exclude=None, verbose=True):
 			word_vecs_vad[index][-3]   = 5
 			word_vecs_vad[index][-2]   = 1
 			word_vecs_vad[index][-1]   = 5
-
-	np.save(model_path,word_vecs_vad)
 
 	if verbose:
 		sys.stderr.write("{}/{} words assigned corresponsing VAD values.\n".format(count_vad, len(vocab2int)))
@@ -148,3 +164,24 @@ def aff2vec(model_path, vocab2int, aff_embeddings_path="./w2_counterfit_append_a
 		sys.stderr.write("{}/{} words assigned the neutral vector.\n".format(len(assign_neutral), len(vocab2int)))
 
 	return word_vecs_emot
+
+if __name__ == "__main__":
+	parser = create_parser()
+	args = parser.parse_args()
+
+	word2int = {}
+	with open(args.vocab, "r", encoding="utf-8") as r:
+		for line in r.readlines():
+			[word, index] = line.split()
+			word2int[word] = int(index)
+
+	summation = sum(bool(option) for option in [args.w2v, args.retrofit, args.counterfit])
+	if summation != 1:
+		raise ValueError("You must choose exactly one of --w2v, --retrofit, or --counterfit")
+
+	if args.w2v:
+		with open(args.w2v, "r", encoding="utf-8") as r:
+			tokens = [line.split() for line in r.readlines()]
+		embeddings = w2vec(tokens, word2int, embedding_size=args.size) 
+	np.save(args.save_path, embeddings)
+
